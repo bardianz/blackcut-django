eb70345dc60c4a7c6b093be17402530ee9554181
Bardia Nikbakhsh <bardianikbakhsh@gmail.com>
2024-04-25 12:48:28 +0330

    allow any rrq for api

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import ActiveAppointments,AllProducts,add_product_to_appointment,remove_product_from_appointment
+from .views import ActiveAppointments,AllProducts,AddProductToAppointment,remove_product_from_appointment
 
 urlpatterns = [
     path('allproducts/', AllProducts.as_view(), name='allproducts'),
     path('activeappointments/', ActiveAppointments.as_view(), name='activeappointments'),
-    path('add_product_to_appointment/', add_product_to_appointment, name='add_product_to_appointment'),
+    path('add_product_to_appointment/', AddProductToAppointment.as_view(), name='add_product_to_appointment'),
     path('remove_product_from_appointment/', remove_product_from_appointment, name='remove_product_from_appointment'),
 ]
--- api/views.py
+++ api/views.py
@@ -27,20 +27,20 @@ class AllProducts(APIView):
         return Response(serializer.data)
 
 
-@api_view(['POST'])
-def add_product_to_appointment(request):
-    appointment_id = request.data.get('appointment_id')
-    product_id = request.data.get('product_id')
-    try:
-        appointment = Appointment.objects.get(id=appointment_id)
-        product = Product.objects.get(id=product_id)
-        appointment.products.add(product)
-        appointment.save()
-        return Response({'message': 'Product added successfully'},status=status.HTTP_200_OK)
-    except Appointment.DoesNotExist:
-        return Response({'message': 'Appointment does not exist'},status=status.HTTP_404_NOT_FOUND)
-    except Product.DoesNotExist:
-        return Response({'message': 'Product does not exist'},status=status.HTTP_404_NOT_FOUND)
+class AddProductToAppointment(APIView):
+    def post(self, request):
+        appointment_id = request.data.get('appointment_id')
+        product_id = request.data.get('product_id')
+        try:
+            appointment = Appointment.objects.get(id=appointment_id)
+            product = Product.objects.get(id=product_id)
+            appointment.products.add(product)
+            appointment.save()
+            return Response({'message': 'Product added successfully'}, status=status.HTTP_200_OK)
+        except Appointment.DoesNotExist:
+            return Response({'message': 'Appointment does not exist'}, status=status.HTTP_404_NOT_FOUND)
+        except Product.DoesNotExist:
+            return Response({'message': 'Product does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['POST'])
 def remove_product_from_appointment(request):
