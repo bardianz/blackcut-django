45582489e49a82ff9deed3a252eb4f3f3ffff791
Bardia Nikbakhsh <bardianikbakhsh@gmail.com>
2024-09-25 01:13:55 +0330

    fix discount code button on mobile view and add collapse for it

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product,Category
 # Register your models here.
 
 
@@ -7,3 +7,9 @@ class ProductAdmin(admin.ModelAdmin):
      empty_value_display = "-هیچ آیتمی وجود ندارد-"
 
 admin.site.register(Product,ProductAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+     empty_value_display = "-هیچ دسته بندی‌ای وجود ندارد-"
+
+admin.site.register(Category,CategoryAdmin)
--- shop/models.py
+++ shop/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 from PIL import Image
 
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, verbose_name="عنوان دسته بندی")
+    rank = models.IntegerField(verbose_name='رتبه قرارگیری',default=0,)
+
+    class Meta:
+        verbose_name = "دسته بندی"
+        verbose_name_plural = "دسته بندی ها"
+
+    def __str__(self) -> str:
+        return self.title
+
 class Product(models.Model):
     name = models.CharField(max_length=50, verbose_name="نام محصول")
-    img = models.ImageField(default="shop/no-picture.png", upload_to='shop/product/images/', verbose_name="تصویر محصول")
+    img = models.ImageField(default="reservation//no-picture.png", upload_to='shop/product/images/', verbose_name="تصویر محصول")
     price = models.IntegerField(verbose_name='قیمت به تومان',default=0,)
+    quantity = models.IntegerField(verbose_name='تعداد',default=0  , null=True,blank=True)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,default=None,null=True,blank=True)
+
 
 
     def save(self, *args, **kwargs):
