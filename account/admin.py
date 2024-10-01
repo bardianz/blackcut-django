0ec8eb04c9257d37d56a0c9b005ca65cff335ea5
Bardia Nikbakhsh <bardianikbakhsh@gmail.com>
2024-10-01 18:21:57 +0330

    a few changes on admin.py

--- account/admin.py
+++ account/admin.py
@@ -3,23 +3,29 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from .models import UserProfile
 from django.utils.html import mark_safe
+from django.urls import reverse
 
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
     can_delete = False
     verbose_name_plural = 'پروفایل کاربران'
 
-# ترکیب اطلاعات کاربر و پروفایل
 class CustomUserAdmin(UserAdmin):
     inlines = (UserProfileInline,)
-    list_display = ('display_profile_picture' , 'first_name', 'last_name','username', 'get_phone_number' ,'email', )
+    list_display = ('display_profile_picture', 'full_name_link', 'username', 'get_phone_number', 'email')
+
+    def full_name_link(self, obj):
+        full_name = f"{obj.first_name} {obj.last_name}"
+        edit_url = reverse('admin:auth_user_change', args=[obj.id])  # لینک به صفحه ویرایش کاربر
+        return mark_safe(f'<a href="{edit_url}">{full_name}</a>')
+
+    full_name_link.short_description = 'نام کامل'
 
     def get_phone_number(self, obj):
         return obj.userprofile.phone_number if obj.userprofile else 'ندارد'
 
     get_phone_number.short_description = 'شماره تلفن'
 
-    # نمایش تصویر پروفایل
     def display_profile_picture(self, obj):
         if hasattr(obj, 'userprofile') and obj.userprofile.profile_picture:
             return mark_safe(f'<img src="{obj.userprofile.profile_picture}" width="50" height="50" />')
@@ -28,6 +34,9 @@ class CustomUserAdmin(UserAdmin):
 
     display_profile_picture.short_description = 'تصویر پروفایل'
 
-# حذف ثبت قبلی مدل User و ثبت مجدد با inline
+
+User._meta.verbose_name = 'کاربر'
+User._meta.verbose_name_plural = 'کاربران'
+
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
