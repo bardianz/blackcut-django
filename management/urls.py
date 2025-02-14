a059290205db3f30db4d0e6f63ec4c1beae14057
Bardia <Bardianikbakhsh@gmail.com>
2025-02-14 18:41:39 +0330

    add done page for management

--- management/urls.py
+++ management/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from .views import list_of_active_reservations,list_of_all_reservations
+from .views import list_of_active_reservations,list_of_all_reservations,list_of_done_reservations
 
 app_name =  "management"
 urlpatterns = [
 
     path('', list_of_active_reservations, name="list_of_reservations"),
     path('all/', list_of_all_reservations, name="list_of_all_reservations"),
+    path('done/', list_of_done_reservations, name="list_of_done_reservations"),
 ]
--- management/views.py
+++ management/views.py
@@ -52,3 +52,27 @@ def list_of_all_reservations(request):
               "page":"all",
          }
         return render(request,template_name,context)
+
+
+def list_of_done_reservations(request):
+        if not (request.user.is_superuser or request.user.is_staff):
+            messages.error(request, 'شما دسترسی ندارید')
+            return redirect("account:login")
+        
+        template_name = "management/management.html"
+
+        current_date = date.today()
+        all_reservations = Appointment.objects.filter(status="done").order_by('date', 'timeslot__start_time')
+        active_reservations= []
+        for appointment in all_reservations:
+            if appointment.status == "active":
+                if appointment.date < current_date:
+                    appointment.status="expired"
+                    appointment.save()
+            active_reservations.append(appointment)
+
+        context = {
+              "active_reservations" : active_reservations,
+              "page":"done",
+         }
+        return render(request,template_name,context)
